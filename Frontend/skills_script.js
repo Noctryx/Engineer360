4037ed6484042358185542b5208f8dce6238c5a5
Venky <venky14182007@gmail.com>
2026-02-23 13:58:27 +0530

    Fixed production API URLs

--- Frontend/skills_script.js
+++ Frontend/skills_script.js
@@ -14,7 +14,7 @@ async function loadSkills() {
       return;
     }
 
-    const response = await fetch("https://engineer360.onrender.com/roles");
+    const response = await fetch("https://engineer360.onrender.com/skills");
     const data = await response.json();
 
     const skills = Object.keys(data[role] || {});
